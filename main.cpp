448f66dc8cc279ce84e714a2492fc91260e7ae97
Ryan Bride <rbride@mail.bradley.edu>
2022-09-20 23:49:25 -0400

    Work on fill function

--- main.cpp
+++ main.cpp
@@ -6,42 +6,66 @@
 #include "algorithms.h"
 #include <cstdlib>
 #include <ctime>
-#include <array> 
+#include <vector> 
 #include <algorithm>
 #include <iterator>
 #include <random>
 #include <iostream>
+#include <type_traits>
+
+using std::size_t; 
+using std::vector;
+using std::is_same;
+//Forward Declare Merge Function to keep the same order I have in the my Notes
+template<class T>
+void Merge_Function(vector<T>&, int, int, int);
 
 /**************************************************
-* Random Fill Function, fills an Array    
+* Random Fill Function, fills an vector    
 **************************************************/
-template<class Integer, size_t SIZE>
-void fill_array( std::array<Integer, SIZE>& a, Integer lower = 0, Integer upper = 30000 )
+template<class T>
+void fill_vector( std::vector<T>& a, int lower = 1, int upper = 30000 )
 {
+  T temp;
   static std::random_device rnd;
   static std::mt19937 mte(rnd()); //
-
-
-  std::uniform_int_distribution<Integer> dist(lower, upper);
-  std::generate(a.begin(), a.end(), bind(dist, mte) );
-
+  //std::cout << std::boolalpha << '\n' << is_same<int, decltype(t)>::value << '\n';
+  
+  if( is_same<int, decltype(temp)>::value ){
+      std::uniform_int_distribution<int> dist(lower, upper);
+      a.resize(10000);
+      std::generate(a.begin(), a.end(), std::bind(dist, mte) );
+  }
+  else if( is_same<double, decltype(temp)>::value ){
+      std::cout << "\nFloats are broken\nSad!\n"
+      /*
+      std::uniform_real_distribution<double> dist(1.0, 1000.0);
+      a.resize(10000);
+      std::generate(a.begin(), a.end(), std::bind(dist, mte) );
+      */
+  }
+  else{
+    std::cout << "Can only generate random vector of type int and Double\n";
+  }
+  
+  return;
 }
 
 /**************************************************
 * Merge Sort and Merge functions
 * P and R are assumed to be the Beginning and end of 
-* the Specified Array
+* the Specified vector
 **************************************************/
 // TODO: Design it so that it takes optional inputs for P and R, and if none are given
 //       Specify P and R as Arr.begin() and Arr.End()
-template<class Int_or_Float, size_t Size_Arr>
-void Merge_Sort(std::array<Int_or_Float, Size_Arr>& Arr, int p,  int r) {
-  //Return the Sorted Array if the Array is of size 1
+template<class T>
+void Merge_Sort(vector<T>& Arr, int p,  int r) {
+  //Return the Sorted vector if the vector is of size 1
   if(Arr.size() == 1){
     return;
   }
 
-  size_t midpoint = (Arr.size()-1)/2;
+  int midpoint = (static_cast<int>(Arr.size())-1)/2;
   //Divide in 2 and recursively call
   Merge_Sort(Arr, p, midpoint);
   Merge_Sort(Arr, midpoint+1, r);
@@ -52,16 +76,17 @@ void Merge_Sort(std::array<Int_or_Float, Size_Arr>& Arr, int p,  int r) {
   return; 
 }
 
-template <class Int_or_Float, size_t Size_Arr>
-void Merge_Function(std::array<Int_or_Float, Size_Arr>& Arr, int p, int midpoint, int r){
-  /* Get the Length of Each Subarray*/
+template <class T>
+void Merge_Function(vector<T>& Arr, int p, int midpoint, int r){
+  /* Get the Length of Each Subvector*/
   int lsa1 = midpoint - p + 1;
   int lsa2 = r - midpoint;
-
-  /* Create two seperate subarrays with 1 extra space each*/
-  left = std::array<Int_or_Float, lsa1+1>;
-  std::array<Int_or_Float, lsa2+1>right;
   
+  /* Create two seperate subvectors with 1 extra space each*/
+  vector<T>left_arr;
+  vector<T>right_arr;
+
+
 
   return;
 }
@@ -70,22 +95,27 @@ void Merge_Function(std::array<Int_or_Float, Size_Arr>& Arr, int p, int midpoint
 
 int main(){
   /* consider using generate and a vector in the second interation of this file*/
-  std::array<int, 10000> merge_sort_test_array;
-  fill_array(merge_sort_test_array);
+  vector<int> test;
+  fill_vector(test);
+  // std::copy(std::begin(test), std::end(test), 
+  //           std::ostream_iterator<int>(std::cout, "\t"));
+  
+  vector<int> test1 = {71,50,57,61,21,12,24,51,20,7,
+                       2,91,11,36,71,21,78,56,8,14,
+                       90,25,28,29,32 };
 
-  std::array<int, 25> test1 = {71,50,57,61,21,12,24,51,20,7,
-                               2,91,11,36,71,21,78,56,8,14,
-                               90,25,28,29,32 };
-  Merge_Sort(test1, 0, test1.size()-1);
+
+  Merge_Sort(test1, 0, static_cast<int>(test1.size())-1);
   std::copy(std::begin(test1), std::end(test1), 
             std::ostream_iterator<int>(std::cout, "\n"));
+  
   std::cout << '\n';
 
-  std::array<double, 5> test2 = {31.120, 8.145, .0005, 17.1};
+  vector<double> test2 = {31.120, 8.145, .0005, 17.1};
   //Merge_Sort(test2, 0, test2.size()-1);
 
   //std::cout << '\n';
-  std::array<double, 1> test3 = {1.1556};
+  vector<double> test3 = {1.1556};
   //Merge_Sort(test3, 0, test3.size()-1);
   
   
@@ -94,7 +124,7 @@ int main(){
 }
 
 
-// Print array
+// Print vector
 //  std::copy(std::begin(Arr),
 //           std::end(Arr),
 //           std::ostream_iterator<Int_or_Float>(std::cout, "\n"));
